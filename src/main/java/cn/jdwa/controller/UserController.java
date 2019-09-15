e9a0ccb28e551243ceea89d4397cbb01782ca8fe
caozhou_zch <caozhou_zch@163.com>
2019-09-15 15:50:29 +0800

    传统方式文件上传

--- src/main/java/cn/jdwa/controller/UserController.java
+++ src/main/java/cn/jdwa/controller/UserController.java
@@ -1,7 +1,15 @@
 package cn.jdwa.controller;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by czz on 2019/9/15.
@@ -11,8 +19,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @RequestMapping("/testFileUpload01")
-    public String testFileUpload01(){
+    public String testFileUpload01(HttpServletRequest request) throws Exception{
         System.out.println("testFileUpload01 ... ... ...");
+        String path = request.getSession().getServletContext().getRealPath("/upload/");
+        System.out.println("path:"+path);
+        File file = new File(path);
+        if (!file.exists()) file.mkdirs();
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        List<FileItem> items = upload.parseRequest(request);
+        for (FileItem item:items){
+            if (!item.isFormField()){
+                String fileName = item.getName();
+                String uuid = UUID.randomUUID().toString().replace("-", "");
+                fileName = uuid + "_" +fileName;
+                System.out.println("fileName:"+fileName);
+                item.write(new File(path,fileName));
+                item.delete();
+            }
+        }
         return "success";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,9 +8,13 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>首页</title>
+    <title>文件上传</title>
 </head>
 <body>
     <h3>首页</h3>
+    <form action="user/testFileUpload01" method="post" enctype="multipart/form-data" >
+        浏览文件<input type="file" name="upload"/></br>
+        <input type="submit" value="上传">
+    </form>
 </body>
 </html>
